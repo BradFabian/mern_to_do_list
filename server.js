a2055825899bc04117cb641c8de4dbc1d39b2b7b
Brad <bradleofabian@gmail.com>
2020-01-25 22:57:39 -0500

    added some comments

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ mongoose
 app.use("/api/todos", todos);
 
 const port = process.env.PORT || 5000;
-
+// express listen to port 5000 or env file port and log which port it starts on
 app.listen(port, () => console.log(`Server started on port ${port}`));
